860aba0ccd8c45e06a2e7ac43cd098b0950c5fc3
Jasmeet Singh <jasmeet5108k@gmail.com>
2023-12-23 09:15:53 +0530

    Created nodes.js for child, parent nodes

---
+++ nodes.js
@@ -0,0 +1,23 @@
+console.log("Hello World");
+        //     let cont = document.body.childNodes[1]
+        // undefined
+        // cont
+        // <div class="container">…</div>
+        // cont.firstChild
+        // #text
+        // cont.firstElementChild
+        // <div class="box">Box1</div>
+        // cont.lastElementChild
+        // <div class="box">Box5</div>
+        // cont.lastElementChild.style.color
+        // ''
+        // cont.lastElementChild.style.color = "red
+        // VM617:1 Uncaught SyntaxError: Invalid or unexpected token
+        // cont.lastElementChild.style.color = "red"
+        // 'red'
+        // cont.lastElementChild.parentElement
+        // <div class="container">…</div>
+        // document.body.firstElementChild
+        // <div class="container">…</div>
+        // document.body.firstElementChild.childNodes
+        // NodeList(11) [text, div.box, text, div.box, text, div.box, text, div.box, text, div.box, text]
