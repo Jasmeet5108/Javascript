26b0bc4925365a428bc795e7c831b60719039e43
Jasmeet Singh <jasmeet5108k@gmail.com>
2023-12-23 09:17:29 +0530

    Removed comment slashes

--- nodes.js
+++ nodes.js
@@ -1,23 +1,23 @@
 console.log("Hello World");
-        //     let cont = document.body.childNodes[1]
+             let cont = document.body.childNodes[1]
         // undefined
         // cont
         // <div class="container">…</div>
-        // cont.firstChild
+         cont.firstChild
         // #text
-        // cont.firstElementChild
+         cont.firstElementChild
         // <div class="box">Box1</div>
-        // cont.lastElementChild
+         cont.lastElementChild
         // <div class="box">Box5</div>
-        // cont.lastElementChild.style.color
+         cont.lastElementChild.style.color
         // ''
-        // cont.lastElementChild.style.color = "red
+         cont.lastElementChild.style.color = "red
         // VM617:1 Uncaught SyntaxError: Invalid or unexpected token
-        // cont.lastElementChild.style.color = "red"
+         cont.lastElementChild.style.color = "red"
         // 'red'
-        // cont.lastElementChild.parentElement
+         cont.lastElementChild.parentElement
         // <div class="container">…</div>
-        // document.body.firstElementChild
+         document.body.firstElementChild
         // <div class="container">…</div>
-        // document.body.firstElementChild.childNodes
+         document.body.firstElementChild.childNodes
         // NodeList(11) [text, div.box, text, div.box, text, div.box, text, div.box, text, div.box, text]
